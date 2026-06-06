13aa58f9917bc3e8cdd81051115e47e79a573486
jhoe-cmd <jhonino@outlook.com>
2026-06-06 11:28:45 -0300

    Adicionando configuração de caminho para Streamlit

--- src/reservatorio/app.py
+++ src/reservatorio/app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+# Estas linhas abaixo garantem que o Python ache a pasta 'src'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
